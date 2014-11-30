228e9ebbd5b3e4c1ae2eafb38033f98e98198e2a
Libor Vozdek <libor.vozdek@gmail.com>
2014-12-01 00:21:33 +0100

    dataloader done

--- MassiveInteractiveGraph.DataLoader/Program.cs
+++ MassiveInteractiveGraph.DataLoader/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.ServiceModel.Configuration;
 using System.Text;
 using System.Threading.Tasks;
-using MassiveInteractiveGraph.DataLoader.ServiceReference1;
+using System.Xml;
+using MassiveInteractiveGraph.DataLoader.DataManagementService;
 
 namespace MassiveInteractiveGraph.DataLoader
 {
@@ -11,21 +14,57 @@ namespace MassiveInteractiveGraph.DataLoader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("testing...");
-            while (Console.ReadKey().Key == ConsoleKey.Enter)
+            var service = new DataManagementService.DataManagementClient();
+            var activeNodes = service.ListActiveNodes();
+            var activeLinks = service.ListActiveLinks();
+
+            var importedNodes = new List<DataManagementService.DataManagementNode>();
+            var importedLinks = new List<DataManagementService.DataManagementLink>();
+
+            var dir = args[0];
+
+            if (Directory.Exists(dir))
             {
-                try
+                var files = Directory.GetFiles(dir);
+                foreach (var file in files)
                 {
-                    var service = new Service1Client();
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(file);
 
-                    var data = service.GetData(0);
+                    var id = int.Parse(doc.SelectSingleNode("/node/id").InnerText);
+                    var label = doc.SelectSingleNode("/node/label").InnerText;
 
-                    Console.WriteLine(data);
+                    importedNodes.Add(new DataManagementNode() {Id = id, Label = label});
+                    
+                    XmlNodeList adjacentIdNodes = doc.SelectNodes("/node/adjacentNodes/id");
+                    foreach (XmlNode adjacentIdNode in adjacentIdNodes)
+                    {
+                        var adjacentId = int.Parse(adjacentIdNode.InnerText);
+                        importedLinks.Add(new DataManagementLink() { Id1 = id, Id2 = adjacentId });
+                    }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
+            }
+
+            var nodesToAdd = importedNodes.Where(n => !activeNodes.Exists(a => a.Id == n.Id)).ToList();
+            var nodesToRename = importedNodes.Where(n => activeNodes.Exists(a => a.Id == n.Id) && activeNodes.Find(a => a.Id == n.Id).Label != n.Label).ToList();
+            var nodesToDelete = activeNodes.Where(n => !importedNodes.Exists(a => a.Id == n.Id)).ToList();
+
+            var linksToAdd = importedLinks.Where(l => !activeLinks.Exists(a => a.Id1 == l.Id1 && a.Id2 == l.Id2)).ToList();
+            var linksToDelete = activeLinks.Where(l => !importedLinks.Exists(a => a.Id1 == l.Id1 && a.Id2 == l.Id2)).ToList();
+
+            try
+            {
+                service.RenameNodes(nodesToRename);
+
+                service.RemoveLinks(linksToDelete);
+                service.RemoveNodes(nodesToDelete);
+
+                service.AddNodes(nodesToAdd);
+                service.AddLinks(linksToAdd);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
